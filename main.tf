1b16669096d9c50283f86888791002362f0db0ac
anubhav <anubhavsrivastava85@gmail.com>
2025-08-04 21:41:44 +0530

    terraform files updated_v1.1

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.92"
-        }
-    }
-    required_version = ">=1.2"
-}
-
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "new_instance" {
+  ami = var.ami
+  instance_type = var.instance_type
+  associate_public_ip_address = true
 }
