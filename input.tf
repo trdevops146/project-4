3b55ce06d375fcee73d204bef776fc5ae2b221a2
anubhav <anubhavsrivastava85@gmail.com>
2025-08-04 21:48:11 +0530

    terraform files updated_v1.2

---
+++ input.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+  description = "this variable will hold the ami"
+  type = string
+}
+variable "instance_type" {
+  description = "this value will hold the instance type"
+  type = string
+}
