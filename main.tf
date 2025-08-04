5df9d2ef13c150c439b8cd0c06b72b64b704e46d
anubhav <anubhavsrivastava85@gmail.com>
2025-08-04 16:37:38 +0530

    update_v1.2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
     required_providers {
         source = "hashicorp/aws"
-        version = ">=5.9.2"
+        version = "~>5.92"
     }
     required_version = ">=1.2"
 }
@@ -10,3 +10,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "terraform_server" {
+    instance_type = "t2.micro"
+    ami = "ami-08a6efd148b1f7504"
+
+}
