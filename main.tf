bd67b4dd4c8fec9663655ac6c06977a16e5a9cca
anubhav <anubhavsrivastava85@gmail.com>
2025-08-04 21:38:52 +0530

    terraform files updated

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
 terraform {
     required_providers {
-        source = "hashicorp/aws"
-        version = "~>5.92"
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.92"
+        }
     }
     required_version = ">=1.2"
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_instance" "terraform_server" {
-    instance_type = "t2.micro"
-    ami = "ami-08a6efd148b1f7504"
-
 }
